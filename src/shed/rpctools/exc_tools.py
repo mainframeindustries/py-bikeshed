76175555b6960fd8506927432773a28a14e15f2c
Kristján Valur Jónsson <sweskman@gmail.com>
2022-06-30 11:10:25 +0000

    Dynamically create an ExceptionType for unknown exceptions

--- src/shed/rpctools/exc_tools.py
+++ src/shed/rpctools/exc_tools.py
@@ -15,20 +15,49 @@ class RemoteException(Exception):
 
     @classmethod
     def FromTracebackException(cls, tbe):
-        # If we provide a single argument which isn't somethign simple, then it doesn't
-        # get formatted with the excetpion.  So, provide two
+        # If we provide a single argument which isn't something simple, then it doesn't
+        # get formatted with the exception.  So, provide two
         result = cls(tbe, list(tbe.format()))
         return result
 
     @classmethod
     def FromStrings(cls, strings):
-        # If we provide a single argument which isn't somethign simple, then it doesn't
-        # get formatted with the excetpion.  So, provide two
+        # If we provide a single argument which isn't something simple, then it doesn't
+        # get formatted with the exception.  So, provide two
         result = cls(list(strings))
         return result
 
 
+class FakeException(type):
+    """
+    Represent an exc_type of a TracebackException which isn't found here.
+    TracebackException.format() expects `exc_type` to be a real type.
+    """
+
+    @classmethod
+    def create(cls, module, name, reprstr):
+        """
+        Dynamically create a new exception type to be used.
+        """
+        fake = cls(name, (Exception,), {})
+        fake.repr = reprstr
+        fake.__module__ = module
+        fake.__qualname__ = module + "." + name
+        return fake
+
+
 # Methods to serialize / deserialize tracebackexceptions
+
+_traceback_exception_attrs = ["__suppress_context__", "_str"]
+_traceback_exception_syntax_attrs = [
+    "filename",
+    "lineno",
+    "end_lineno",
+    "text",
+    "offset",
+    "end_offset",
+    "msg",
+]
 
 
 def traceback_exception_serialize(te: traceback.TracebackException) -> dict:
@@ -36,30 +65,42 @@ def traceback_exception_serialize(te: traceback.TracebackException) -> dict:
         "type": "TracebackException:1.0",
         "__cause__": traceback_exception_serialize(te.__cause__) if te.__cause__ else None,
         "__context__": traceback_exception_serialize(te.__context__) if te.__context__ else None,
-        "__suppress_context__": te.__suppress_context__,
         "stack": stack_summary_serialize(te.stack),
         "exc_type": exc_type_serialize(te.exc_type),
     }
-    for name in ["filename", "lineno", "text", "offset", "msg"]:
-        result[name] = getattr(te, name, None)
+    for name in _traceback_exception_attrs:
+        result[name] = getattr(te, name)
+    if issubclass(te.exc_type, SyntaxError):
+        se = {}
+        for name in _traceback_exception_syntax_attrs:
+            se[name] = getattr(te, name, None)
+        result["syntax_error"] = se
+    else:
+        result["syntax_error"] = None
     return result
 
 
 def traceback_exception_deserialize(te: dict) -> traceback.TracebackException:
-    # construct a dummy TracebackException
+    tbtype = te.get("type", "TracebackException:1.0")
+    assert tbtype in ["TracebackException:1.0"]
+
+    # construct a dummy TracebackException and fill its atributes
     try:
         raise RuntimeError()
     except RuntimeError as e:
         result = traceback.TracebackException.from_exception(e)
-    tbtype = te.get("type", "TracebackException:1.0")
-    assert tbtype in ["TracebackException:1.0"]
+    else:
+        result = None  # to paciy linters
     result.__cause__ = traceback_exception_deserialize(te["__cause__"]) if te["__cause__"] else None
     result.__context__ = traceback_exception_deserialize(te["__context__"]) if te["__context__"] else None
     result.stack = stack_summary_deserialize(te["stack"])
     result.exc_type = exc_type_deserialize(te["exc_type"])
-
-    for name in ["__suppress_context__", "filename", "lineno", "text", "offset", "msg"]:
+    for name in _traceback_exception_attrs:
         setattr(result, name, te[name])
+    if te.get("syntax_error"):
+        for name in _traceback_exception_syntax_attrs:
+            value = te["syntax_error"].get(name)
+            setattr(result, name, value)
     return result
 
 
@@ -79,7 +120,7 @@ def exc_type_deserialize(exc_type: dict):
         mod = importlib.import_module(exc_type["module"])
         return getattr(mod, exc_type["name"])
     except (ImportError, AttributeError):
-        return exc_type["repr"]
+        return FakeException.create(exc_type["module"], exc_type["name"], exc_type["repr"])
 
 
 def stack_summary_serialize(stack_summary: traceback.StackSummary) -> list:
